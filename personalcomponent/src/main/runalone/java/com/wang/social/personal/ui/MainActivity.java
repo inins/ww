873ee748f7c7960f5e9e78c9b438c6cbf46d95ff
jainchen <jaincjing@gmail.com>
2018-03-28 14:14:45 +0800

    单独运行组件配置

---
+++ personalcomponent/src/main/runalone/java/com/wang/social/personal/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wang.social.personal.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wang.social.personal.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_activity_main);
+    }
+}
