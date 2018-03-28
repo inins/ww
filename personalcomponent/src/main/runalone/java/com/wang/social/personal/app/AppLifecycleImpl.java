ee90cf423ac0dd3f126b2e1dd14ba4ca0812e52d
jainchen <jaincjing@gmail.com>
2018-03-28 14:32:53 +0800

    每个组件都使用LeakCanary检测内存泄漏

--- personalcomponent/src/main/runalone/java/com/wang/social/personal/app/AppLifecycleImpl.java
+++ personalcomponent/src/main/runalone/java/com/wang/social/personal/app/AppLifecycleImpl.java
@@ -4,6 +4,9 @@ import android.app.Application;
 import android.content.Context;
 
 import com.frame.base.delegate.AppDelegate;
+import com.frame.utils.FrameUtils;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 import com.wang.social.personal.BuildConfig;
 
 import butterknife.ButterKnife;
@@ -24,11 +27,18 @@ public class AppLifecycleImpl implements AppDelegate{
 
     @Override
     public void onCreate(Application application) {
+        if (LeakCanary.isInAnalyzerProcess(application)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
         if (BuildConfig.LOG_DEBUG){
             Timber.plant(new Timber.DebugTree());
 
             ButterKnife.setDebug(true);
         }
+        //LeakCanary检查内存泄漏
+        FrameUtils.obtainAppComponentFromContext(application).extras().put(RefWatcher.class.getName(), BuildConfig.USE_CANARY ? LeakCanary.install(application) : RefWatcher.DISABLED);
     }
 
     @Override
