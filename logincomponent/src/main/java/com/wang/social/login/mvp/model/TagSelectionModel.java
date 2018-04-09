a36c5d6142369d7dfb34a31e505509ee36e2cd90
chocfun <25099591@qq.com>
2018-04-09 09:43:05 +0800

    -- 准备更新代码

--- logincomponent/src/main/java/com/wang/social/login/mvp/model/TagSelectionModel.java
+++ logincomponent/src/main/java/com/wang/social/login/mvp/model/TagSelectionModel.java
@@ -1,5 +1,6 @@
 package com.wang.social.login.mvp.model;
 
+import com.frame.component.common.NetParam;
 import com.frame.di.scope.ActivityScope;
 import com.frame.http.api.BaseJson;
 import com.frame.integration.IRepositoryManager;
@@ -8,6 +9,8 @@ import com.wang.social.login.mvp.contract.TagSelectionContract;
 import com.wang.social.login.mvp.model.api.TagService;
 import com.wang.social.login.mvp.model.entities.dto.Tags;
 import com.wang.social.login.mvp.model.entities.dto.TagsDTO;
+
+import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -22,8 +25,11 @@ public class TagSelectionModel extends BaseModel implements TagSelectionContract
 
     @Override
     public Observable<BaseJson<TagsDTO>> passwordLogin() {
+        Map<String, Object> param = new NetParam()
+                .put("v","2.0.0")
+                .build();
         return mRepositoryManager
                 .obtainRetrofitService(TagService.class)
-                .parentTagList();
+                .parentTagList("2.0.0");
     }
 }
--- logincomponent/src/main/java/com/wang/social/login/mvp/model/api/TagService.java
+++ logincomponent/src/main/java/com/wang/social/login/mvp/model/api/TagService.java
@@ -3,13 +3,15 @@ package com.wang.social.login.mvp.model.api;
 import com.frame.http.api.BaseJson;
 import com.wang.social.login.mvp.model.entities.dto.TagsDTO;
 
+import java.util.Map;
+
 import io.reactivex.Observable;
-import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.POST;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 
 public interface TagService {
 
-    @FormUrlEncoded
-    @POST("app/tag/parentTagList")
-    Observable<BaseJson<TagsDTO>> parentTagList();
+    @GET("app/tag/parentTagList")
+    Observable<BaseJson<TagsDTO>> parentTagList(@Query("v") String version);
 }
