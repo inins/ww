0ea4a24fcbabb0593ddb45482728d921aec48416
jainchen <jaincjing@gmail.com>
2018-04-21 18:29:18 +0800

    个人会话页面添加

---
+++ imcomponent/src/main/java/com/wang/social/im/mvp/ui/PrivateConversationActivity.java
@@ -0,0 +1,32 @@
+package com.wang.social.im.mvp.ui;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+
+import com.frame.component.ui.base.BasicAppActivity;
+import com.frame.di.component.AppComponent;
+
+/**
+ * ============================================
+ * 个人会话界面
+ * <p>
+ * Create by ChenJing on 2018-04-21 18:05
+ * ============================================
+ */
+public class PrivateConversationActivity extends BasicAppActivity{
+
+    @Override
+    public void setupActivityComponent(@NonNull AppComponent appComponent) {
+
+    }
+
+    @Override
+    public int initView(@NonNull Bundle savedInstanceState) {
+        return 0;
+    }
+
+    @Override
+    public void initData(@NonNull Bundle savedInstanceState) {
+
+    }
+}
