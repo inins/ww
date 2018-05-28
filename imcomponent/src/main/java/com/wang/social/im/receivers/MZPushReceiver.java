827478d8b1c1dfcd01794ccd422b587ae1d33b0a
jainchen <jaincjing@gmail.com>
2018-05-28 21:07:24 +0800

    魅族推送修改

--- imcomponent/src/main/java/com/wang/social/im/receivers/MZPushReceiver.java
+++ imcomponent/src/main/java/com/wang/social/im/receivers/MZPushReceiver.java
@@ -1,9 +1,11 @@
 package com.wang.social.im.receivers;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 
 import com.meizu.cloud.pushsdk.MzPushMessageReceiver;
+import com.meizu.cloud.pushsdk.handler.MzPushMessage;
 import com.meizu.cloud.pushsdk.platform.message.PushSwitchStatus;
 import com.meizu.cloud.pushsdk.platform.message.RegisterStatus;
 import com.meizu.cloud.pushsdk.platform.message.SubAliasStatus;
@@ -43,7 +45,7 @@ public class MZPushReceiver extends MzPushMessageReceiver {
 
     @Override
     public void onRegisterStatus(Context context, RegisterStatus registerStatus) {
-        Timber.tag(TAG).d("onRegisterStatus:" + registerStatus.getPushId());
+        Timber.tag(TAG).d("onRegisterStatus:" + registerStatus.getCode() + "," +registerStatus.getPushId());
         //上报 busiid 和 pushid 到腾讯云，需要在登录成功后进行上报
         TIMOfflinePushToken token = new TIMOfflinePushToken(IMConstants.MZPUSH_BUSSID, registerStatus.getPushId());
         TIMManager.getInstance().setOfflinePushToken(token, new TIMCallBack() {
@@ -72,5 +74,41 @@ public class MZPushReceiver extends MzPushMessageReceiver {
     @Override
     public void onSubAliasStatus(Context context, SubAliasStatus subAliasStatus) {
 
+    }
+
+    @Override
+    public void onNotificationArrived(Context context, MzPushMessage mzPushMessage) {
+        super.onNotificationArrived(context, mzPushMessage);
+        Timber.tag(TAG).d("onNotificationArrived:"+mzPushMessage.getContent());
+    }
+
+    @Override
+    public void onNotifyMessageArrived(Context context, String s) {
+        super.onNotifyMessageArrived(context, s);
+        Timber.tag(TAG).d("onNotifyMessageArrived:"+s);
+    }
+
+    @Override
+    public void onNotificationClicked(Context context, MzPushMessage mzPushMessage) {
+        super.onNotificationClicked(context, mzPushMessage);
+        Timber.tag(TAG).d("onNotificationClicked:"+mzPushMessage.getContent());
+    }
+
+    @Override
+    public void onMessage(Context context, String s) {
+        super.onMessage(context, s);
+        Timber.tag(TAG).d("onMessage:"+s);
+    }
+
+    @Override
+    public void onMessage(Context context, Intent intent) {
+        super.onMessage(context, intent);
+        Timber.tag(TAG).d("onMessage:intent");
+    }
+
+    @Override
+    public void onMessage(Context context, String s, String s1) {
+        super.onMessage(context, s, s1);
+        Timber.tag(TAG).d("onMessage:s1 "+s);
     }
 }
