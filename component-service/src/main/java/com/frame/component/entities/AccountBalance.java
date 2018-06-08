a6e084696f9349ae4023a283faec3fa989451c7d
liaoinstan <liaoinstan@qq.com>
2018-06-08 20:46:40 +0800

    fix bugs

--- component-service/src/main/java/com/frame/component/entities/AccountBalance.java
+++ component-service/src/main/java/com/frame/component/entities/AccountBalance.java
@@ -5,11 +5,15 @@ import lombok.Data;
 @Data
 public class AccountBalance {
     private float amount;
-    private int amountGemstone;
+    private float amountGemstone;
     private int userId;
     private float amountDiamond;
 
     public int getAmountDiamond() {
         return (int) amountDiamond;
+    }
+
+    public int getAmountGemstone() {
+        return (int)amountGemstone;
     }
 }
