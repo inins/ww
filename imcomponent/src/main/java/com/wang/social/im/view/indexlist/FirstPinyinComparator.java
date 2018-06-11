66759885a6a7d9f498760ab4514012a6c07391af
jainchen <jaincjing@gmail.com>
2018-06-11 17:52:56 +0800

    修改排序规则

--- imcomponent/src/main/java/com/wang/social/im/view/indexlist/FirstPinyinComparator.java
+++ imcomponent/src/main/java/com/wang/social/im/view/indexlist/FirstPinyinComparator.java
@@ -2,7 +2,9 @@ package com.wang.social.im.view.indexlist;
 
 import android.support.annotation.NonNull;
 
+import java.text.Collator;
 import java.util.Comparator;
+import java.util.Locale;
 
 /**
  * ============================================
@@ -26,9 +28,19 @@ public class FirstPinyinComparator<T extends IndexableEntity> implements Compara
 
     private int compareIndexName(String lhs, String rhs) {
         int index = 0;
-        String lhsWord = getWord(lhs, index);
-        String rhsWord = getWord(rhs, index);
-        return lhsWord.compareTo(rhsWord);
+        String lhsWord = getOriWord(lhs, index);
+        String rhsWord = getOriWord(rhs, index);
+
+        Collator collator = Collator.getInstance(Locale.CANADA);
+        int flag;
+        if (collator.compare(lhsWord, rhsWord) < 0) {
+            flag = -1;
+        } else if (collator.compare(lhsWord, rhsWord) > 0) {
+            flag = 1;
+        } else {
+            flag = 0;
+        }
+        return flag;
     }
 
     @NonNull
@@ -41,5 +53,13 @@ public class FirstPinyinComparator<T extends IndexableEntity> implements Compara
             firstWord = PinyinUtil.getPingYin(indexName.substring(index, index + 1));
         }
         return firstWord;
+    }
+
+    @NonNull
+    private String getOriWord(String indexName, int index) {
+        if (indexName.length() < (index + 1)) {
+            return "";
+        }
+        return indexName.substring(index, index + 1);
     }
 }
